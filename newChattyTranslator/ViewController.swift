8b88a92eba9a1c35ea2d961262fc53d19978e28d
evergeenmouse <evergreenmouse@gmail.com>
2019-04-07 21:24:54 +0300

    Initial Commit

---
+++ newChattyTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newChattyTranslator
+//
+//  Created by Александр Ополовников on 07/04/2019.
+//  Copyright © 2019 evergreenmouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
